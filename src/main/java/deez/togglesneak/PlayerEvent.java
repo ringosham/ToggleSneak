be0891afc9a8dd0c416ce5e87193b116aa8f9ec6
Ringo Sham <ringosham@gmail.com>
2020-08-25 13:09:11 +0100

    Partial implementation

--- src/main/java/deez/togglesneak/PlayerEvent.java
+++ src/main/java/deez/togglesneak/PlayerEvent.java
@@ -1,16 +1,27 @@
 package deez.togglesneak;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.GameSettings;
+import net.minecraft.init.MobEffects;
+import net.minecraft.util.MovementInputFromOptions;
 import net.minecraftforge.client.event.InputUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.fml.common.API;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.text.DecimalFormat;
 
 public class PlayerEvent {
     public static final PlayerEvent instance = new PlayerEvent();
     private static boolean isUseItem = false;
+    private boolean sprintHeldAndReleased;
+    private boolean isDisabled;
+    private boolean sprintDoubleTapped;
+    private boolean sprint;
 
-    private PlayerEvent() {}
+    private PlayerEvent() {
+    }
 
     @SubscribeEvent
     public void onUseItemStart(LivingEntityUseItemEvent.Start event) {
@@ -43,5 +54,51 @@ public class PlayerEvent {
 
     public boolean isUseItem() {
         return isUseItem;
+    }
+
+    public void UpdateSprint(boolean newValue, boolean doubleTapped) {
+        this.sprint = newValue;
+        this.sprintDoubleTapped = doubleTapped;
+    }
+
+    private void updateStatus(MovementInputFromOptions options, EntityPlayerSP thisPlayer, GameSettings settings) {
+        if (ToggleSneakMod.optionShowHUDText) {
+            String output = "";
+
+            boolean isFlying = thisPlayer.capabilities.isFlying;
+            boolean isRiding = thisPlayer.isRiding();
+            boolean isHoldingSneak = settings.keyBindSneak.isKeyDown();
+            boolean isHoldingSprint = settings.keyBindSprint.isKeyDown();
+            boolean isElytraFlying = thisPlayer.isElytraFlying();
+
+            if (isFlying) {
+                DecimalFormat numFormat = new DecimalFormat("#.00");
+                if (ToggleSneakMod.optionEnableFlyBoost && isHoldingSprint)
+                    output += "[Flying (" + numFormat.format(ToggleSneakMod.optionFlyBoostAmount) + "x boost)]  ";
+                else output += "[Flying]  ";
+            }
+
+            if (isElytraFlying)
+                output += "[Elytra flying]  ";
+
+            if (isRiding) output += "[Riding]  ";
+
+            if (options.sneak) {
+                if (isFlying) output += "[Descending]  ";
+                else if (isRiding) output += "[Dismounting]  ";
+                else if (isHoldingSneak) output += "[Sneaking (Key Held)]  ";
+                else output += "[Sneaking (Toggled)]  ";
+            } else if (thisPlayer.isSprinting()) {
+                if (!isFlying && !isRiding) {
+                    //  Detect Vanilla conditions - ToggleSprint disabled, DoubleTapped and Hold & Release
+                    boolean isVanilla = this.sprintHeldAndReleased || isDisabled || this.sprintDoubleTapped;
+
+                    if (isHoldingSprint) output += "[Sprinting (Key Held)]";
+                    else if (isVanilla) output += "[Sprinting (Vanilla)]";
+                    else output += "[Sprinting (Toggled)]";
+                }
+            }
+            RenderTextToHUD.SetHUDText(output);
+        }
     }
 }
--- src/main/java/deez/togglesneak/ToggleSneakEvents.java
+++ src/main/java/deez/togglesneak/ToggleSneakEvents.java
@@ -1,18 +1,22 @@
 package deez.togglesneak;
 
+import deez.togglesneak.gui.GuiOptionsReplace;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.gui.GuiOptions;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import deez.togglesneak.gui.GuiOptionsReplace;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import org.lwjgl.input.Keyboard;
 
 public class ToggleSneakEvents
 {
 	public static ToggleSneakEvents instance = new ToggleSneakEvents();
 
 	protected static final Minecraft mc = Minecraft.getMinecraft();
+
+	private long startPressTime = 0;
+	private long endPressTime = 0;
 	
 	@SubscribeEvent
 	public void GuiOpenEvent(GuiOpenEvent event)
@@ -20,6 +24,22 @@ public class ToggleSneakEvents
 		if(event.getGui() instanceof GuiOptions && mc.world != null)
 		{
 			event.setGui(new GuiOptionsReplace(new GuiIngameMenu(), mc.gameSettings));
+		}
+	}
+
+	//KeyInputEvent only fires when the first key down occurs, it does not fire on subsequent key downs.
+	@SubscribeEvent
+	public void onKeyDown(InputEvent.KeyInputEvent event) {
+		if (Keyboard.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak.getKeyCode())) {
+			endPressTime = System.currentTimeMillis();
+			System.out.println("Sneak key down");
+		} else {
+			System.out.println("Sneak key up");
+			if (endPressTime != 0) {
+				System.out.println("Sneak hold time: " + (endPressTime - startPressTime) + "ms");
+				endPressTime = 0;
+			}
+			startPressTime = System.currentTimeMillis();
 		}
 	}
 }
