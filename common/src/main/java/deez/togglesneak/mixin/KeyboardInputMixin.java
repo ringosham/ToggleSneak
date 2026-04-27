42844c0aced98272e30ae7183382ae61ef5acf08
Ringo Sham <ringosham@gmail.com>
2026-04-27 23:37:34 +0100

    Fix build system

--- common/src/main/java/deez/togglesneak/mixin/KeyboardInputMixin.java
+++ common/src/main/java/deez/togglesneak/mixin/KeyboardInputMixin.java
@@ -4,7 +4,7 @@ import deez.togglesneak.hud.Status;
 import net.minecraft.client.Options;
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.world.entity.player.Input;
-import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +12,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin {
+    @Final
     @Shadow
-    public Options options;
+    private Options options;
 
-    @Redirect(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/player/KeyboardInput;keyPresses:Lnet/minecraft/world/entity/player/Input;", opcode = Opcodes.PUTFIELD))
+    // For opcodes, see org.objectweb.asm.Opcodes
+    @Redirect(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/player/KeyboardInput;keyPresses:Lnet/minecraft/world/entity/player/Input;", opcode = 181))
     public void newInput(KeyboardInput input, Input keyPresses) {
         input.keyPresses = new Input(
                 this.options.keyUp.isDown(),
